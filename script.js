197f097fe8741f9abcbb49c57b59074bdf377788
Abeen Khanal <abeenkhanal16@gmail.com>
2024-06-19 21:53:44 +0545

    add

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-function navbar(){
-    
+
+let navitems=document.querySelectorAll('items ul li')
+
+function navbar(active){
+    console.log(navitems)
+    navitems.forEach((li)=>{
+        console.log(li.getAttribute('id')==active);
+       li.classlist.remove('active')
+       if(li.getAttribute('id')==active){
+        li.classlist.add('active')
+       }
+    })
 }
+navbar('home')
